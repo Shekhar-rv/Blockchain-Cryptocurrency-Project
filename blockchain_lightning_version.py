951292b9faa3aec0f8a926c04de29aaf90a9f94b
Shekhar Ramesh <shekhar.rv@gmail.com>
2020-04-18 13:31:43 +0100

    Creating a class Block chain

--- blockchain_lightning_version.py
+++ blockchain_lightning_version.py
@@ -1,4 +1,11 @@
-# Just a preview of how the blockchain works.
+# This a sample code of how the blockchain works.
+
+# Create a function that a simple hash method using *
+
+def lightning_hash(data):
+    return data + '*'
+
+# Create a class Block that takes 3 arguments 
 
 class Block:
     def __init__(self, data, hash, last_hash):
@@ -6,4 +13,40 @@ class Block:
         self.hash = hash
         self.last_hash = last_hash
 
+# Create a variable with an instance of class Block
+
 foo_block = Block('foo_data', 'foo_hash', 'foo_last_hash')
+
+# Print out the data
+
+print(foo_block.data)
+print(foo_block.hash)
+print(foo_block.last_hash)
+
+# Create a class Blockchain that creates a genesis block and adds the new blocks to the genesis block
+
+class Blockchain:
+    def __init__(self):
+        genesis = Block('gen_data', 'gen_hash', 'gen_last_hash')
+
+        self.chain = [genesis]
+
+# Creating a function append the new blocks to the genesis block after hashing
+
+    def add_block(self, data):
+        last_hash = self.chain[-1].hash
+        hash = lightning_hash(data + last_hash)
+
+        block = Block(data, hash, last_hash)
+
+        self.chain.append(block)
+
+# Creating a 
+
+foo_blockchain = Blockchain()
+foo_blockchain.add_block('one')
+foo_blockchain.add_block('two')
+foo_blockchain.add_block('three')
+
+for block in foo_blockchain.chain:
+    print(block.__dict__)
