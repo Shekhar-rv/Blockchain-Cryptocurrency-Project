d7814eacfe72412a7c51f23aed2366dfc00c5f4a
Shekhar Ramesh <shekhar.rv@gmail.com>
2020-04-21 19:58:11 +0100

    Added mine_block, genesis block and changes to class Block

--- block.py
+++ block.py
@@ -1,18 +1,44 @@
+import time
+
+def mine_block(last_block, data):
+    """
+    Mine a block based on the given last_block and data.
+    """
+    timestamp = time.time_ns()
+    last_hash = last_block.hash
+    hash = f"{timestamp}-{last_hash}"
+
+    return Block(timestamp, last_hash, hash, data)
+
+def genesis():
+    """
+    Generate the genesis block.
+    """
+    return Block(1, 'genesis_last_hash', 'genesis_hash', [])
+
 class Block:
     """
     Block: a unit of storage.
     Storage transactions in a blockchain that supports a crytocurrency.
     """
-    def __init__(self, data):
+    def __init__(self, timestamp, last_hash, hash, data):
+        self.timestamp = timestamp
+        self.last_hash = last_hash
+        self.hash = hash
         self.data = data
 
     def __repr__(self):
-        return f'Block-data: {self.data}'
+        return ( 
+            f'timestamp: {self.timestamp}, '
+            f'last_hash: {self.last_hash}, '
+            f'hash: {self.hash}, '
+            f'Block-data: {self.data}'
+        )
 
 def main():
-    block = Block('foo')
+    genesis_block = genesis()
+    block = mine_block(genesis_block, 'foo')
     print(block)
-    print(f"block.py __name__: {__name__}") # Check the name of the module
 
 # If the name of the module is equal to '__main__' then it will run the main function
 
