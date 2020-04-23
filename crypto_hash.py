5eecfa976abd32e10f39d268b7138ddb44370fe9
Shekhar Ramesh <shekhar.rv@gmail.com>
2020-04-23 19:56:00 +0100

    Creating the hashing algorithm

--- crypto_hash.py
+++ crypto_hash.py
@@ -6,7 +6,8 @@ import json
 def crypto_hash(data):
     """
     Return a sha-256 hash of the given data.
-    To get the sha-256 hash you need to encode the data as utf-8 and the call the hexdigest funcion on it.
+    To get the sha-256 hash you need to encode the data as utf-8 to convert it to a byte chain and the call the hexdigest funcion.
+    To see the unqiue string output we need to use hexdigest() otherwise you get the memory address representation.
     """
     stringified_data = json.dumps(data) # Dumps the data regardless of its type into a str representation
 
