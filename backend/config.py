eb56949296cba1224d862b1f0ce155e2fab40228
Shekhar Ramesh <shekhar.rv@gmail.com>
2020-04-28 19:44:22 +0100

    Creating a dynamic difficulty and Mine rate and testing

---
+++ backend/config.py
@@ -0,0 +1,10 @@
+# This file will collect global values for the project 
+
+# All Global values will be in uppercase
+
+NANOSECONDS = 1
+MICROSECONDS = 1000 * NANOSECONDS
+MILLISECONDS = 1000 * MICROSECONDS
+SECONDS = 1000 * MILLISECONDS
+
+MINE_RATE = 4 * SECONDS
