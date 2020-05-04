e0e492a313112a586c1b006f5ba788f0795a425a
Shekhar Ramesh <shekhar.rv@gmail.com>
2020-05-04 14:32:27 +0100

    Proof of work implementation

--- backend/blockchain/block.py
+++ backend/blockchain/block.py
@@ -36,6 +36,9 @@ class Block:
             f'nonce: {self.nonce}'
         )
 
+    def __eq__(self, other):
+        return self.__dict__ == other.__dict__
+
     @staticmethod # Setting the function under a class, the function does not have a self keyword so we use a decorator static method
     def mine_block(last_block, data):
         """
@@ -76,14 +79,48 @@ class Block:
 
         return 1
 
+    @staticmethod
+    def is_valid_block(last_block, block):
+        """
+        Validate block by enforcing the following rules:
+            - the block must have the proper last_hash reference
+            - the block must meet the proof of work requirement
+            - the difficulty must only adjust by 1
+            - the block hash must be a valid combiantion of the block fields
+        """
+
+        if block.last_hash != last_block.hash:
+            raise Exception("The block last_hash must be correct")
+
+        if hex_to_binary(block.hash)[0:block.difficulty] != '0' * block.difficulty:
+            raise Exception("The proof of work requirement was not met")
+
+        if abs(last_block.difficulty - block.difficulty) > 1:
+            raise Exception("The block difficulty must only adjust by 1")
+
+        reconstructed_hash = crypto_hash(
+            block.timestamp,
+            block.last_hash,
+            block.data,
+            block.nonce,
+            block.difficulty
+        )
+
+        if block.hash != reconstructed_hash:
+            raise Exception("The block hash must be correct")
+
 def main():
     genesis_block = Block.genesis()
-    block = Block.mine_block(genesis_block, 'foo')
-    print(block)
+    good_block = Block.mine_block(genesis_block, 'foo')
+    # bad_block.last_hash = 'evil_data'
+
+    try: 
+        Block.is_valid_block(genesis_block, good_block)
+    except Exception as e:
+        print(f'is_valid_block: {e}')
 
 # If the name of the module is equal to '__main__' then it will run the main function
 
 if __name__ == '__main__':
     main()
 
-    
