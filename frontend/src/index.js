484aa7ccbf16e2310965765c73df16ac0c1670f7
Shekhar Ramesh <shekhar.rv@gmail.com>
2020-06-14 14:13:21 +0100

    Frontend Cryptocurrency

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Switch, Route } from 'react-router-dom';
+import history from './history';
 import './index.css';
 import App from './components/App';
+import Blockchain from './components/Blockchain';
+import ConductTransaction from './components/ConductTransaction';
+import TransactionPool from './components/TransactionPool';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<Router history={history}>
+		<Switch>
+			<Route path='/' exact component={App} />
+			<Route path='/blockchain' component={Blockchain} />
+			<Route path='/conducttransaction' component={ConductTransaction} />
+			<Route path='/transactionpool' component={TransactionPool} />
+		</Switch>
+	</Router>, 
+	document.getElementById('root')
+	);
 
 
